399f10298cc3437b738a4739f0ddb5c6cd1e7d09
Vedat A <155557999+vedudi@users.noreply.github.com>
2024-05-11 00:49:13 +0300

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
 export const statusOpt=["Mülakat","Devam Ediyor","Reddedildi"]
 export const typeOpt=["Tam Zamanlı", "Yarı Zamanlı","Uzaktan","Staj"]
-export const sortOpt = ['a-z', 'z-a', 'En Yeni', 'En Eski'];
+export const sortOpt = ['A-Z', 'Z-A', 'En Yeni', 'En Eski'];
